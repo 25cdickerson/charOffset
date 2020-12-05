866d68e38d930a4566cc1c86e126824a6ceac1c0
Christopher Dickerson <christopher.dickers1@sinclair.edu>
2020-12-05 14:15:52 -0500

    Decided to put the try catch block in the main function. Made classes for the throw exceptions. Added the tests in the main.

--- charOffset/charOffset.cpp
+++ charOffset/charOffset.cpp
@@ -9,28 +9,40 @@
 
 using namespace std;
 
-char character(char start, int offset) {
-	// Have an error in my try catch block
-	try {
-		start = start + offset;
+class invalidCharacterException {};
+class invalidRangeException {};
 
-		if (!isalpha(start + offset)) {
-			throw "Invaild Range Exception";
-		}
-		else if (!isalpha(start)) {
-			throw "Invalid Character Exception";
-		}
+char character(char start, int offset) {
+
+	start = start + offset;
+
+
+	if (!isalpha(start)) {
+		throw invalidCharacterException();
+	}
+	else if (!isalpha(start + offset)) {
+		throw invalidRangeException();
+	}
 		
-		return start;
-	}
-	catch (string s) {
-		cout << "ERROR:" << s;
-	}
+	return start;
 }
 
 int main()
 {
-	cout << character('a', -1);
+	try {
+		cout << character('a', 1) << "\n";
+		cout << character('a', -1) << "\n";
+		cout << character('Z', -1) << "\n";
+		cout << character('?', 5) << "\n";
+	}
+	catch (invalidCharacterException) {
+		cout << "ERROR: Invalid Character\n";
+	}
+	catch (invalidRangeException) {
+		cout << "ERROR: Invalid Range\n";
+	}
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
